cf73f3c1ac32b68926ffcda1c0ee478372a8eb71
Eyob <eyget73@gmail.com>
2026-09-25 15:20:40 +0300

    fix: revert apiUrl to use relative path to resolve SameSite cookie issues

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://inventory-management-w5ya.onrender.com/api',
+  apiUrl: '/api',
   vapidPublicKey: 'BAH97f8svkNpXqRZOBKhnaqP8Nor0H-0PiCPCN0BzQNnSh9MsfJENJojSffZROFGlzIkzeBHjlakZ7c3_xdnfCQ',
 };
